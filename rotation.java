f80bd9a56e668a2566022aa822710e952ebd7057
Akki747 <bairapuakshaya@gmail.com>
2024-12-07 16:21:56 +0530

    Add files via upload

--- rotation.java
+++ rotation.java
@@ -1,16 +1,15 @@
 import java.util.*;
 public class rotation
 {
-    public static void main(String args[])
+    public static void main(String args[]) 
     {
-        Scanner sc=new Scanner(System.in);
-        int()nums={1,2,3,4,5,6,7,9}
+        int[]nums={1,2,3,4,5,6,7,9};
         int k=3;
         int n=nums.length;
         int temp[]=new int[n];
         for(int i=0;i<n;i++)
         {
-            temp((i+k)%n)=nums[i];
+            temp[(i+k)%n]=nums[i];
         }
         System.arraycopy(temp,0,nums,0,n);
         for(int i=0;i<nums.length;i++)
