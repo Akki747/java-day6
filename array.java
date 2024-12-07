10ed9a9455e62cb2f077cae1ad0bcee329310121
Akki747 <bairapuakshaya@gmail.com>
2024-12-07 16:06:07 +0530

    Add files via upload

--- array.java
+++ array.java
@@ -3,12 +3,12 @@ class array
 {
     public static void main(String args[])
     {
-        Scanner s=new Scanner(System.in);
-        int n=s.nextInt();
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
         int arr[]=new int[n];
         for(int i=0;i<n;i++)
         {
-            arr[i]=s.nextInt();
+            arr[i]=sc.nextInt();
         }
         System.out.println(Arrays.toString(arr));
 
